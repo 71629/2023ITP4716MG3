cfbf80327bf22796b7a1025e6c63331610694820
71629 <105049686+71629@users.noreply.github.com>
2023-04-09 22:35:09 +0800

    Audio settings

--- WildRunnerUI/WildRunnerUI/Assets/Scripts/GameManager.cs
+++ WildRunnerUI/WildRunnerUI/Assets/Scripts/GameManager.cs
@@ -16,7 +16,11 @@ public class GameManager : MonoBehaviour
 
     GameObject LastSelectedGameObject;
     [SerializeField] GameObject FirstSelect;
+
     [SerializeField] Slider MasterVolume, MusicVolume, VoiceVolume, SFXVolume;
+    [SerializeField] Text[] ValueDisplay;
+    [SerializeField] Text VolumeReset;
+    [SerializeField] GameObject VolumeResetProgress;
 
     private void Start()
     {
@@ -83,21 +87,124 @@ public class GameManager : MonoBehaviour
     private void UpdateMasterVolume()
     { 
         PlayerPrefs.SetFloat("MasterVolume", MasterVolume.value);
+        MasterVolume.value = PlayerPrefs.GetFloat("MasterVolume");
+        ValueDisplay[0].text = (PlayerPrefs.GetFloat("MasterVolume") * 100).ToString("0") + "%";
+        if(PlayerPrefs.GetFloat("MasterVolume") == 0) 
+        { 
+            ValueDisplay[0].color = new Color(0.5f, 0.2f, 0.2f);
+            ValueDisplay[0].text = "MUTE";
+        }
+        if(PlayerPrefs.GetFloat("MasterVolume") > 1)
+        {
+            ValueDisplay[0].color = new Color(0, 1, 1);
+        }
+        if (PlayerPrefs.GetFloat("MasterVolume") <= 1 && PlayerPrefs.GetFloat("MasterVolume") != 0)
+        {
+            ValueDisplay[0].color = new Color(1, 1, 1);
+        }
     }
 
     private void UpdateMusicVolume()
     { 
         PlayerPrefs.SetFloat("MusicVolume", MusicVolume.value);
+        MusicVolume.value = PlayerPrefs.GetFloat("MusicVolume");
+        ValueDisplay[1].text = (PlayerPrefs.GetFloat("MusicVolume") * PlayerPrefs.GetFloat("MasterVolume") * 100).ToString("0") + "%";
+        if (PlayerPrefs.GetFloat("MusicVolume") * PlayerPrefs.GetFloat("MasterVolume") == 0)
+        {
+            ValueDisplay[1].color = new Color(0.5f, 0.2f, 0.2f);
+            ValueDisplay[1].text = "MUTE";
+        }
+        if (PlayerPrefs.GetFloat("MusicVolume") * PlayerPrefs.GetFloat("MasterVolume") > 1)
+        {
+            ValueDisplay[1].color = new Color(0, 1, 1);
+        }
+        if (PlayerPrefs.GetFloat("MusicVolume") * PlayerPrefs.GetFloat("MasterVolume") <= 1 && PlayerPrefs.GetFloat("MusicVolume") * PlayerPrefs.GetFloat("MasterVolume") != 0)
+        {
+            ValueDisplay[1].color = new Color(1, 1, 1);
+        }
     }
 
     private void UpdateVoiceVolume()
     { 
         PlayerPrefs.SetFloat("VoiceVolume", VoiceVolume.value);
+        VoiceVolume.value = PlayerPrefs.GetFloat("VoiceVolume");
+        ValueDisplay[2].text = (PlayerPrefs.GetFloat("VoiceVolume") * PlayerPrefs.GetFloat("MasterVolume") * 100).ToString("0") + "%";
+        if (PlayerPrefs.GetFloat("VoiceVolume") * PlayerPrefs.GetFloat("MasterVolume") == 0)
+        {
+            ValueDisplay[2].color = new Color(0.5f, 0.2f, 0.2f);
+            ValueDisplay[2].text = "MUTE";
+        }
+        if (PlayerPrefs.GetFloat("VoiceVolume") * PlayerPrefs.GetFloat("MasterVolume") > 1)
+        {
+            ValueDisplay[2].color = new Color(0, 1, 1);
+        }
+        if (PlayerPrefs.GetFloat("VoiceVolume") * PlayerPrefs.GetFloat("MasterVolume") <= 1 && PlayerPrefs.GetFloat("VoiceVolume") * PlayerPrefs.GetFloat("MasterVolume") != 0)
+        {
+            ValueDisplay[2].color = new Color(1, 1, 1);
+        }
     }
 
     private void UpdateSFXVolume()
     { 
         PlayerPrefs.SetFloat("SFXVolume", SFXVolume.value);
+        SFXVolume.value = PlayerPrefs.GetFloat("SFXVolume");
+        ValueDisplay[3].text = (PlayerPrefs.GetFloat("SFXVolume") * PlayerPrefs.GetFloat("MasterVolume") * 100).ToString("0") + "%";
+        if (PlayerPrefs.GetFloat("SFXVolume") * PlayerPrefs.GetFloat("MasterVolume") == 0)
+        {
+            ValueDisplay[3].color = new Color(0.5f, 0.2f, 0.2f);
+            ValueDisplay[3].text = "MUTE";
+        }
+        if (PlayerPrefs.GetFloat("SFXVolume") * PlayerPrefs.GetFloat("MasterVolume") > 1)
+        {
+            ValueDisplay[3].color = new Color(0, 1, 1);
+        }
+        if (PlayerPrefs.GetFloat("SFXVolume") * PlayerPrefs.GetFloat("MasterVolume") <= 1 && PlayerPrefs.GetFloat("SFXVolume") * PlayerPrefs.GetFloat("MasterVolume") != 0)
+        {
+            ValueDisplay[3].color = new Color(1, 1, 1);
+        }
+    }
+
+    public void ResetVolume()
+    {
+        StartCoroutine(VolumeChangeEffect());
+    }
+
+    IEnumerator VolumeChangeEffect()
+    {
+        yield return new WaitForEndOfFrame();
+
+        if (MasterVolume.value == 1 && MusicVolume.value == 1 && VoiceVolume.value == 1 && SFXVolume.value == 1)
+        {
+            VolumeReset.text = "Reset to default";
+            MasterVolume.interactable = true;
+            MusicVolume.interactable = true;
+            VoiceVolume.interactable = true;
+            SFXVolume.interactable = true;
+            VolumeResetProgress.GetComponent<Slider>().value = 0;
+            VolumeResetProgress.SetActive(false);
+        }
+        else
+        {
+            StartCoroutine(VolumeChangeEffect());
+            VolumeReset.text = "Adjusting";
+            MasterVolume.interactable = false;
+            MusicVolume.interactable = false;
+            VoiceVolume.interactable = false;
+            SFXVolume.interactable = false;
+            VolumeResetProgress.SetActive(true);
+        }
+
+        MasterVolume.value += (1 - MasterVolume.value) * Time.deltaTime * 5;
+        MusicVolume.value += (1 - MusicVolume.value) * Time.deltaTime * 5;
+        VoiceVolume.value += (1 - VoiceVolume.value) * Time.deltaTime * 5;
+        SFXVolume.value += (1 - SFXVolume.value) * Time.deltaTime * 5;
+
+        if(MasterVolume.value >= 0.999f && MasterVolume.value <= 1.005f) { MasterVolume.value = 1; }
+        if(MusicVolume.value >= 0.999f && MusicVolume.value <= 1.005f) { MusicVolume.value = 1; }
+        if(VoiceVolume.value >= 0.999f && VoiceVolume.value <= 1.005f) { VoiceVolume.value = 1; }
+        if(SFXVolume.value >= 0.999f && SFXVolume.value <= 1.005f) { SFXVolume.value = 1; }
+
+        VolumeResetProgress.GetComponent<Slider>().value += (1 - VolumeResetProgress.GetComponent<Slider>().value) * Time.deltaTime * 5;
     }
 
     public static void QuitGame() //Quit Game
