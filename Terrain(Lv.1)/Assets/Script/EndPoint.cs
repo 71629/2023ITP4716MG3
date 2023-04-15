a14f57a492f22f92ccf4f881f404478c390d6530
moonlet023 <moonlet023@gmail.com>
2023-04-15 14:02:01 +0800

    odds&ends
    https://www.youtube.com/watch?v=HUzLUGKwQJc

---
+++ Terrain(Lv.1)/Assets/Script/EndPoint.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+using UnityEditor;
+
+public class EndPoint : ShowUI
+{   
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "Player";      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EndPoint"))
+        {
+            Debug.Log("touch end point");
+            showEndUI();
+        }
+    }
+
+}
