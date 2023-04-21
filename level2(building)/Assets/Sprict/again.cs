43d83a2c18b7e0a7eb3dc944c37f777428ecf4b3
moonlet023 <moonlet023@gmail.com>
2023-04-21 10:48:30 +0800

    building(講你知123)

--- level2(building)/Assets/Sprict/again.cs
+++ level2(building)/Assets/Sprict/again.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class again : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.tag = "Player";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < 7)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
-   
+
+    public static void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
