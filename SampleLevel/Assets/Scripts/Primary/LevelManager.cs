9a216c1e086213c1a0faafceebcb320bf2c813cc
Tang Tsz Hin <105049686+71629@users.noreply.github.com>
2023-05-18 09:18:20 +0800

    Small Update

--- SampleLevel/Assets/Scripts/Primary/LevelManager.cs
+++ SampleLevel/Assets/Scripts/Primary/LevelManager.cs
@@ -8,11 +8,14 @@ public class LevelManager : MonoBehaviour
 	[SerializeField] float BaseScore, TotalScore;
 	[SerializeField] float Time, StandardTime;
 	float[] Timer = new float[3];
+	gameObject Player;
+	Animator Canvas;
 	
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+		Player = GameObject.FindWithTag("Player");
+		Canvas = GameObject.FindWithTag("Canvas").GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -23,26 +26,35 @@ public class LevelManager : MonoBehaviour
 
 	public void StartLevel()
 	{
-		
+		// Start the timer and show UIs
+		// TODO: Design new UIs
+		Canvas.SetTrigger("Start");
 	}
 	
 	// FinishLevel is called once the player enters the finish zone
 	public void FinishLevel()
 	{
-		
+		// Calculate the score
+		// TODO: Add a Time Bonus mechanic calculated with the StandardTime
+		TotalScore = BaseScore + (Time - StandardTime);
+		// Display the score
+		Canvas.SetTrigger("Finish");
 	}
 	
 	public void RestartLevel()
 	{
-		
+		// * Changes might be required
+		Canvas.SetTrigger("Restart");
 	}
 	
 	public void QuitLevel()
 	{
-		
+		// * Changes might be required
+		Canvas.SetTrigger("Quit");
 	}
 	
 	private void OnTriggerEnter(Collider other) {
+		// If the player enters the finish zone
 		if(other.gameObject.CompareTag("Finish"))
 		{
 			FinishLevel();
